11660717eab8add6e068fdea260af59e7dee199b
carlos3lb <carlos3lb@hotmail.com>
2017-11-12 23:22:48 +0000

    Extended closures and collections

--- src/groovyFundamentals/basics/Closures.groovy
+++ src/groovyFundamentals/basics/Closures.groovy
@@ -8,3 +8,9 @@ for (i in 1..3) { myClosure() sleep 1000}
 (1..3).each {i -> print i}
 println''
 (1..10).findAll({it%2==0}).each({print it})
+
+def comparator = {a, b -> a == b ? 0 : (a < b ? -1 : 1)}
+def list = [7, 4, 9, -6, -1, 11, 2, 3, -9, 5, -13]
+println''
+println list.max(comparator)
+println list.min(comparator)
--- src/groovyFundamentals/basics/Collections.groovy
+++ src/groovyFundamentals/basics/Collections.groovy
@@ -2,3 +2,14 @@ def galacticos = ["Zidane", "Beckham", "Figo", "Ronaldo"]
 def minus = "-"
 galacticos.each {println "$minus $it"}
 println "Best: ${galacticos[2]}"
+
+println galacticos.size()
+println galacticos[-1]
+galacticos.eachWithIndex {galactico, index -> println "${index+1}: $galactico"}
+
+println ((1..10).collect{it*2})
+println ((1..10).find{it<5})
+println ((1..10).findAll{it<5})
+println ((1..10).sum())
+println ((1..10).sum(1000))
+println ((1..10).max())
